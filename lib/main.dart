576ea3fbc96576bd609b414848ffaf69e7c30784
nhung2312 <18t1021229@husc.edu.vn>
2021-11-18 19:43:21 +0700

    bmi

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,35 @@
-import 'package:buoi2/view/screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'Home.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown]);
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: NewScreen());
+      title: 'Material App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: BmiPage()
+    );
   }
 }
